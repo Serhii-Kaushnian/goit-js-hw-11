ed74e88587184a838e980be1146b4b24a3318cb3
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-11 17:46:58 +0200

    started task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 'use strict';
+import Notiflix from 'notiflix';
+// Notiflix.Notify.warning('Memento te hominem esse');
+
+const refs = {
+  searchBtnRef: document.querySelector('.search-submit'),
+  formRef: document.querySelector('.search-form'),
+};
+
+async function getPhotos(event) {
+  event.preventDefault();
+  try {
+    const response = await fetch(
+      'https://pixabay.com/api/?key=8185021-24268e96be1b2c00462570825&q=yellow+flowers&image_type=photo&pretty=true'
+    );
+    const data = await response.json();
+    console.log(data.totalHits);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+refs.formRef.addEventListener('submit', getPhotos);
